69f3da1b5f94741e7e8bfefdb36e4a5ebde474c2
xinjie-just <18227752005@qq.com>
2017-08-26 10:10:56 +0800

    首页基本样式完成

--- scripts/puyu.js
+++ scripts/puyu.js
@@ -1,3 +1,15 @@
 /**
  * Created by lixinjie on 2017/8/25.
  */
+/*回到顶部*/
+$(window).scroll(function() {
+    if($(window).scrollTop() >= 100) {
+        $("#toTop").fadeIn(); /* 当滑动到不小于 100px 时，回到顶部图标显示 */
+    }else {
+        $("#toTop").fadeOut(); /* 当滑动到小于(页面被卷去的高度) 100px 时，回到顶部图标隐藏 */
+    }
+});
+$("#toTop").click(function () {
+    $("html, body").animate({scrollTop: 0}, 100); /* 持续时间为 100ms */
+    return false;
+});
