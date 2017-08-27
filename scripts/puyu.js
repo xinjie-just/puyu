0b3ce60973c508be1bdb0825d03cb00ebade8d8c
xinjie-just <18227752005@qq.com>
2017-08-27 17:24:28 +0800

    首页pad和phone调整

--- scripts/puyu.js
+++ scripts/puyu.js
@@ -3,13 +3,50 @@
  */
 /*回到顶部*/
 $(window).scroll(function() {
-    if($(window).scrollTop() >= 100) {
-        $("#toTop").fadeIn(); /* 当滑动到不小于 100px 时，回到顶部图标显示 */
+    if($(window).scrollTop() >= 200) {
+        $("#toTop").fadeIn(); /* 当滑动到不小于 200px 时，回到顶部图标显示 */
     }else {
-        $("#toTop").fadeOut(); /* 当滑动到小于(页面被卷去的高度) 100px 时，回到顶部图标隐藏 */
+        $("#toTop").fadeOut(); /* 当滑动到小于(页面被卷去的高度)200px 时，回到顶部图标隐藏 */
     }
 });
 $("#toTop").click(function () {
-    $("html, body").animate({scrollTop: 0}, 100); /* 持续时间为 100ms */
+    $("html, body").animate({scrollTop: 0}, 200); /* 持续时间为 200ms */
     return false;
 });
+
+// 当页面宽度不小于 992px 时，二级菜单显示与隐藏
+if (!($("body").outerWidth() < 992)) {
+    $("#nav ul li").hover(
+        // 导航菜单二级列表缓慢下滑，600ms同css中时间一致
+        function () {
+            $(this).find("ul").slideDown(600);
+        },function () {
+            $(this).find("ul").slideUp(600);
+        }
+    );
+}
+
+// 当页面宽度小于 992px 时，一级菜单显示与隐藏
+if ($("body").outerWidth() < 992) {
+    $("#menuIcon").on("click", function(e){
+        $("#navList").slideToggle(400);
+        $(document).on("click", function(){
+            $("#navList").slideUp();
+        });
+        e.stopPropagation();
+    });
+    $("#navList").on("click", function(e){
+        e.stopPropagation();
+    });
+}
+
+// 微信二维码的显示与影藏
+$("#weixinIcon").hover(
+    function(){
+        $(this).css("color", "#fff");
+        $("#weixinImg").slideDown(400);
+    },function() {
+        $(this).css("color", "#909090");
+        $("#weixinImg").slideUp(400);
+    }
+);
