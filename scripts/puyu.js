8aa8b5660ef5c79ce39d5797d58dde0e641707d9
xinjie-just <18227752005@qq.com>
2017-09-06 22:15:47 +0800

    一些优化

--- scripts/puyu.js
+++ scripts/puyu.js
@@ -14,30 +14,43 @@ $("#toTop").click(function () {
     return false;
 });
 
+
+// 重新加载页面时
+firstGrade();
+secondGrade();
+$(window).resize = function () {
+    firstGrade();
+    secondGrade();
+};
+
 // 当页面宽度不小于 1199px 时，二级菜单显示与隐藏
-if (!($("body").outerWidth() < 1199)) {
-    $("#nav ul li").hover(
-        // 导航菜单二级列表缓慢下滑，600ms同css中时间一致
-        function () {
-            $(this).find("ul").slideDown(600);
-        },function () {
-            $(this).find("ul").slideUp(600);
-        }
-    );
+function secondGrade() {
+    if (!($("body").outerWidth() < 1199)) {
+        $("#nav ul li").hover(
+            // 导航菜单二级列表缓慢下滑，600ms同css中时间一致
+            function () {
+                $(this).find("ul").slideDown(600);
+            },function () {
+                $(this).find("ul").slideUp(600);
+            }
+        );
+    }
 }
 
 // 当页面宽度小于 1199px 时，一级菜单显示与隐藏
-if ($("body").outerWidth() < 1199) {
-    $("#menuIcon").on("click", function(e){
-        $("#navList").slideToggle(400);
-        $(document).on("click", function(){
+function firstGrade() {
+    if ($("body").outerWidth() < 1199) {
+        $("#menuIcon").on("click", function (e) {
+            $("#navList").slideToggle(400);
+            e.stopPropagation();
+        });
+        $(document).on("click", function () {
             $("#navList").slideUp();
         });
-        e.stopPropagation();
-    });
-    $("#navList").on("click", function(e){
-        e.stopPropagation();
-    });
+        $("#navList").on("click", function (e) {
+            e.stopPropagation();
+        });
+    }
 }
 
 // 微信二维码的显示与影藏
@@ -50,13 +63,3 @@ $("#weixinIcon").hover(
         $("#weixinImg").slideUp(400);
     }
 );
-
-// 多行文本省略号
-$(window).load = function () {
-    ellipsis();
-    setWidth();
-};
-$(window).resize = function () {
-    ellipsis();
-    setWidth();
-};
